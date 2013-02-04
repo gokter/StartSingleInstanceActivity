faf9cad9e2774de858f71364970409d7125fc3cd
flycoding <flycoding@yeah.net>
2013-02-04 22:14:30 +0800

    use another app to test single instance launch mode.

---
+++ gen/com/flyingh/startsingleinstanceactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.flyingh.startsingleinstanceactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
